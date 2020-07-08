3b06d2ddc738d65a0e4a58010c62987544b9c2f4
Alin Ciu <alin.ciu@endava.com>
2020-07-08 17:06:20 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('test');
+import { Server, ServerCredentials } from 'grpc';
+
+const server = new Server();
+server.bind('0.0.0.0:3000', ServerCredentials.createInsecure());
+server.start();

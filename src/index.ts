f61e78f296d1c2b4ef08d57e762b0cf8f6c883da
Alin Ciu <alin.ciu@endava.com>
2020-07-10 15:29:33 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,14 @@ const randomInteger = (min: number, max: number): number => {
 const server = new Server();
 server.addService(services.DarkSkyService, {
   getTemp: (call, callback) => {
+    console.log(`Called getTemp() with request`, call.request);
     const request = call.request;
     const reply = new messages.GetTempReply();
     reply.setCurrentTemp(randomInteger(0, 100));
     callback(null, reply);
   },
   getWind: (call, callback) => {
+    console.log(`Called getWind() with request`, call.request);
     const request = call.request;
     const reply = new messages.GetWindReply();
     reply.setCurrentWind(randomInteger(0, 100));
